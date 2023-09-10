85964db6060d03a94a3d8e2450b939d3e5774e46
andrea.nvr <an.nvr@gmail.com>
2023-09-10 19:34:28 +1000

    Update data.js

--- order-app/data.js
+++ order-app/data.js
@@ -4,20 +4,22 @@ const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        picture: "./images/pizza.png"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
+        picture: "./images/hamburger.png",
         id: 1
     },
         {
         name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: ["grain", "hops", "yeast", "water"],
         price: 12,
-        emoji: "🍺",
+        picture: "./images/beer.png",
         id: 2
     }
 ]
+
+export default menuArray
