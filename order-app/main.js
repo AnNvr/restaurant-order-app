1ca7e11220613fe04ecaf853a53baad4149e2080
andrea.nvr <an.nvr@gmail.com>
2023-09-14 21:23:22 +1000

    Update main.js

--- order-app/main.js
+++ order-app/main.js
@@ -1,29 +1,28 @@
 import menuArray from "./data";
 let cartItems = []
-
+const modal = document.getElementById("modal")
+const form = document.getElementById("form")
+const orderCompleteContainer = document.querySelector(".order-complete-container")
+const orderCompleteText = document.getElementById("order-complete-text");
+const totalContainer = document.querySelector(".total-container");
 
 document.addEventListener("click", (e) => {
     const addIcon = e.target.dataset.add
+    const removeItem = e.target.dataset.remove
+    const completeOrderBtn = e.target.dataset.role
+
     if (addIcon){
         addToCart(addIcon)
+    } else if (removeItem){
+        handleDeleteClick(removeItem)
+    } else if (completeOrderBtn){
+        renderModal()
+    } else if (e.target.id === "modal-close-btn"){
+        modal.style.display = "none"
+    } else if (e.target.dataset.role === "pay-btn"){
+        pay()
     }
 })
-
-function getTotalPrice(){
-    return cartItems.reduce((total, item) => total + item.price, 0)
-}
-
-function addToCart(itemId){
-    // convert the itemId to a number
-    const targetItemId = Number(itemId)
-    // find the target item and push it into the cart array
-    const targetItemObj = menuArray.find((item) => item.id === targetItemId)
-    if (targetItemObj){
-        cartItems.push(targetItemObj)
-        renderCart()
-    }
-}
-
 
 function getMenuHtml() {
     let menuHtlm = "";
@@ -36,7 +35,7 @@ function getMenuHtml() {
                     <span class="item-description">
                     <h2 class="item-name">${item.name}</h2>
                     <p class="item-ingredients">${item.ingredients.join(", ")}</p>
-                    <h2 class="item-price">${item.price}</h2>
+                    <h2 class="item-price">$${item.price}</h2>
                     </span>
                 </div>
                 <span class="icon">
@@ -53,33 +52,119 @@ getMenuHtml()
 
 
 
-// display the shopping cart and calculate the total
-// function getCartItemsHtml(){
-//     const total = getTotalPrice()
-//     let cartHtml = ""
-//     cartItems.map((item) => {
-//         cartHtml += `
-//             <div class="cart-item">
-//                 <h2 class="cart-item-name">${item.name}</h2>
-//                 <button class="btn-remove" data-remove=${item.id}>remove</button>
-//                 <p class="cart-item-price">$${item.price}</p>
-//             </div>
-//         `
-//         }).join("")
-//         return cartHtml
-// }
-
-getCartItemsHtml()
-
-// render cart items when an item is added to cart
-function renderCart(){
-    document.getElementById("cart-el").innerHTML = getCartItemsHtml()
+// display the shopping list
+function getCartItemsHtml(){
+    let cartHtml = ""
+    cartItems.map((item) => {
+        cartHtml += `
+            <div class="cart-item">
+                <h2 class="cart-item-name">${item.name}</h2>
+                <button class="btn-remove" data-remove=${item.id}>remove</button>
+                <p class="cart-item-price">$${item.price}</p>
+            </div>
+        `
+        }).join("")
+        
+        return cartHtml
 }
 
+// add to cart mechanics
+function addToCart(itemId){
+    cartItems.push(menuArray[itemId])
+    document.getElementById('cart-el').classList.add('show')
+    renderCart()
+}
+
+// remove item
+function handleDeleteClick(itemId){
+    const itemIndex = cartItems.findIndex(item => item.id === itemId)
+    cartItems.splice(itemIndex, 1)
+
+    if (cartItems.length === 0){
+        document.getElementById("cart-el").classList.remove("show")
+    }
+    renderCart()
+}
+
+// elaborate total
+function getTotalPrice(){
+    // return cartItems.reduce((total, item) => total + item.price, 0)
+
+    const total = cartItems.reduce((totalPrice, item) => {
+        const itemPrice = item.price;
+        const itemID = item.id;
+        // Check if specific item IDs are in the cart together
+        const areIdsInCart = cartItems.some(cartItem =>
+            cartItem.id !== itemID &&
+            (itemID === 0 && cartItem.id === 2) || (itemID === 2 && cartItem.id === 0) ||
+            (itemID === 1 && cartItem.id === 2) || (itemID === 2 && cartItem.id === 1)
+        );
+
+        if (areIdsInCart) {
+            const discount = (20 / 100) * itemPrice; // 20% discount (you can adjust this)
+            totalPrice += itemPrice - discount;
+        } else {
+            totalPrice += itemPrice;
+        }
+
+        return totalPrice;
+    }, 0);
+
+    return total;
+}
+
+// render cart checkout with total
+function renderCart(){
+    const total = getTotalPrice();
+    const cartItemsHtml = getCartItemsHtml();
+    document.getElementById("cart-items").innerHTML = cartItemsHtml;
+    document.getElementById("tot-price").textContent = `$${total}`;
+
+    // discount perk
+    
+    
+    // Check if there are items in the cart and show/hide the total container accordingly
+    if (cartItems.length > 0) {
+        totalContainer.style.display = "block";
+        orderCompleteContainer.style.display = "none"
+    } else {
+        totalContainer.style.display = "none";
+    }
+}
+
+// render modal
+function renderModal(){
+    document.getElementById('cart-el').classList.remove('show')
+    modal.style.display = "block"
+
+}
+
+// form management
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+    const fullName = formData.get("fullName")
+
+    // Update the order confirmation text with the entered name
+    orderCompleteContainer.style.display = "flex"
+    orderCompleteText.textContent = `Thanks, ${fullName}! Your order is on its way!`;
+
+    // Close the modal
+    modal.style.display = "none";
+
+    // Clear up the array from items to start a new order
+    cartItems.length = 0
+    renderCart()
+})
+
+// pay
+function pay() {
+    form.submit()
+}
 
 function renderMenu(){
     document.getElementById("menu-el").innerHTML = getMenuHtml()
 }
 
 renderMenu()
-renderCart()
